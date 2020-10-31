86694c36325959492250a7c1af43ccbf95468aa5
DevJewel <58494211+DevJewel143@users.noreply.github.com>
2020-10-31 19:09:43 +0600

    Add files via upload

---
+++ program_06_jewel.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<fstream>
+#include<iomanip>
+using namespace std;
+void ID()
+{
+    cout<< "Jewel Nath" <<endl;
+    cout<< "ID - 11909037" <<endl;
+    cout<< "Program - 06" <<endl<<endl;
+}
+
+
+int main()
+{
+    ID();
+    string fileName;
+    cout<< "Enter your File Name" <<endl;
+    cout<< "Example : myfile.txt" <<endl;
+    cin>>fileName;
+    ifstream infile;
+    infile.open(fileName);
+    if(infile.fail())
+    {
+        cout<< "Your file didn't work" <<endl;
+        cout<< "Enter a valid file" <<endl;
+    }
+    else
+    {
+        double num;
+        while(infile>>num)
+        {
+            if(num > 100)
+                cout<<num<<endl;
+        }
+    }
+    infile.close();
+    return 0;
+}
+/*
+Sample Input :
+file.txt
+
+Sample Output :
+130
+10002
+776
+*/
+
+
