998584a76336f8338b5c6b57d80bf43b081242c3
DevJewel <58494211+DevJewel143@users.noreply.github.com>
2020-11-05 00:29:01 +0600

    Add files via upload

---
+++ program_21_jewel.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+using namespace std;
+void ID()
+{
+    cout<< "Jewel Nath" <<endl;
+    cout<< "ID - 11909037" <<endl;
+    cout<< "Program - 21" <<endl<<endl;
+}
+class Room
+{
+public:
+    double length;
+    double weight;
+    double height;
+    double Calculate_Area()
+    {
+        return length*weight*height;
+    }
+};
+int main()
+{
+    ID();
+    Room room;
+    cout<< "Enter room's length - ";
+    cin>>room.length;
+    cout<< "Enter room's weight - ";
+    cin>>room.weight;
+    cout<< "Enter room's height - ";
+    cin>>room.height;
+
+    cout<< "Area of this room - " << fixed << setprecision(5) <<room.Calculate_Area() <<endl;
+    return 0;
+}
+/*
+Sample Input :
+
+
+
+Sample Output :
+
+*/
