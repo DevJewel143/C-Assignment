856c66c02098bf29a00027ea74bd0531541c138e
DevJewel <58494211+DevJewel143@users.noreply.github.com>
2020-11-05 01:08:36 +0600

    Add files via upload

---
+++ program_22_jewel.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+void ID()
+{
+    cout<< "Jewel Nath" <<endl;
+    cout<< "ID - 11909037" <<endl;
+    cout<< "Program - 22" <<endl<<endl;
+}
+class A
+{
+
+    private:
+        string name = "Jewel Nath";
+        int ID = 11909037;
+    public:
+        friend class B;
+};
+class B
+{
+    public:
+        void display(A my)
+        {
+            cout<<my.name<<endl;
+            cout<<my.ID<<endl;
+        }
+};
+int main()
+{
+    ID();
+    A object1;
+    B object2;
+
+    object2.display(object1);
+
+    return 0;
+}
+/*
+Sample Input :
+
+Sample Output :
+
+*/
+
