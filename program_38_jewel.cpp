a85551e00d99b2fc3aede09b83d4048fa3a34744
DevJewel <58494211+DevJewel143@users.noreply.github.com>
2020-11-06 21:21:26 +0600

    Add files via upload

---
+++ program_38_jewel.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<sstream>
+#include <exception>
+using namespace std;
+using namespace std;
+
+void ID()
+{
+    cout<< "Jewel Nath" <<endl;
+    cout<< "ID - 11909037" <<endl;
+    cout<< "Program - 38" <<endl<<endl;
+}
+
+class my_class
+{
+    private:
+        int value;
+    public:
+        my_class(int num)
+        {
+            value = num;
+        }
+        void operator ++()
+        {
+            value += 50;
+        }
+        void display()
+        {
+            cout<<"Value = "<<value<<endl;
+        }
+};
+int main()
+{
+    my_class object(10);
+    object.display();
+    ++object;
+    object.display();
+    ++object;
+    object.display();
+}
