44cf3178ec47d1c0e68566e218eb6d4a64fcc0ef
DevJewel <58494211+DevJewel143@users.noreply.github.com>
2020-11-06 21:33:46 +0600

    Add files via upload

---
+++ program_39_jewel.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<sstream>
+#include <exception>
+using namespace std;
+using namespace std;
+
+void ID()
+{
+    cout<< "Jewel Nath" <<endl;
+    cout<< "ID - 11909037" <<endl;
+    cout<< "Program - 39" <<endl<<endl;
+}
+
+class Base
+{
+    public:
+    virtual void print()
+    {
+        cout << "Base Function" << endl;
+    }
+};
+
+class Derived : public Base
+{
+    public:
+    void print()
+    {
+        cout << "Derived Function" << endl;
+    }
+};
+
+int main() {
+    Derived derived;
+
+    Base* base = &derived;
+
+    base->print();
+
+    return 0;
+}
---
+++ program_40_jewel.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<sstream>
+#include <exception>
+using namespace std;
+using namespace std;
+
+void ID()
+{
+    cout<< "Jewel Nath" <<endl;
+    cout<< "ID - 11909037" <<endl;
+    cout<< "Program - 40" <<endl<<endl;
+}
+
+inline int square(int n)
+{
+	return n*n;
+}
+int main()
+{
+	cout << "The square of 15 is: " << square(15) << endl;
+	return 0;
+}
